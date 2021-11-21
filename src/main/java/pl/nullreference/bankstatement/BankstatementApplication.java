8540845023549d4867d1b6f19480f131d07fa5d7
Kamil Delekta <dkamil17@wp.pl>
2021-11-21 19:36:08 +0100

    Adding BankStatementRepository, BankStatementService

--- src/main/java/pl/nullreference/bankstatement/BankstatementApplication.java
+++ src/main/java/pl/nullreference/bankstatement/BankstatementApplication.java
@@ -12,12 +12,17 @@ public class BankstatementApplication implements CommandLineRunner {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
+	@Autowired
+	private BankStatementService bankStatementService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BankstatementApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		bankStatementService.getAllBankStatements()
+				.forEach(System.out::println);
 //		String sql = "INSERT INTO bankstatement (id, date, endbalance, bankname) VALUES (2, '2021-12-12', 4.20, 'Nest Bank')";
 //		int rows = jdbcTemplate.update(sql);
 //		if (rows > 0) {
--- src/main/java/pl/nullreference/bankstatement/model/BankStatement.java
+++ src/main/java/pl/nullreference/bankstatement/model/BankStatement.java
@@ -1,15 +1,26 @@
 package pl.nullreference.bankstatement.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@Entity(name = "bankstatement")
 public class BankStatement {
-    private Date Date;
-    private Double BeginningBalance;
-    private Double EndBalance;
-    private String BankName;
-    private List<BankStatementItem> items;
+    @Id
+    private int id;
+    private Date date;
+//    private Double beginningbalance;
+    private Double endbalance;
+    private String bankname;
+
+    public BankStatement() {
+
+    }
+//    private List<BankStatementItem> items;
 }
